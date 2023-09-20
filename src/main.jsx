c556c3edd266902f40b6e3ef040c7e8ba74eceb4
Supremacy00 <musaabdulmuqaddas@gmail.com>
2023-09-20 16:33:18 -0500

    dependency

--- src/main.jsx
+++ src/main.jsx
@@ -27,9 +27,8 @@ window.addEventListener('unhandledrejection', (event) => {
 
 
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
