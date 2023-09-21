0d5ccd08735ff399a8e2d35e6335c28c08ad348b
Supremacy00 <musaabdulmuqaddas@gmail.com>
2023-09-21 02:37:25 -0500

    bug fixed

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,3 @@
-
 import PropTypes from "prop-types";
 import { MdClose } from "react-icons/md";
 import { useState } from "react";
@@ -19,31 +18,29 @@ const Login = ({ handleClose }) => {
     setIsLoading(true);
     try {
       if (email === "user@example.com" && password === "1Password") {
-      
         const auth = getAuth();
         await signInWithEmailAndPassword(auth, email, password);
         setIsLoading(false);
-        navigate("/homepage"); 
+        navigate("/homepage");
       } else {
         setError("Invalid email or password");
-        setIsLoading(false)
+        setIsLoading(false);
       }
     } catch (error) {
       console.error("Login failed:", error.message);
       setError(error.message);
-      setIsLoading(false)
+      setIsLoading(false);
     }
   };
 
   return (
-
     <div className="fixed flex items-center justify-center z-50 inset-0 font-roboto shadow-3xl shadow-white bg-black bg-opacity-50 backdrop-blur-sm">
       <img
         src="/images/artgallery.jpg"
         className="object-cover hidden rounded-l-lg w-[400px] max-h-[532px] lg:block"
         alt=""
       />
-       <div className="bg-slate-100 rounded-lg shadow-xl relative px-8 py-20 max-h-[432px] mx-6 md:py-[170px]  md:max-h-[532px] lg:mx-0 lg:px-16 lg:rounded-l-none w-[400px] ">
+      <div className="bg-slate-100 rounded-lg shadow-xl relative px-8 py-20 max-h-[432px] mx-6 md:py-[170px]  md:max-h-[532px] lg:mx-0 lg:px-16 lg:rounded-l-none w-[400px] ">
         <div>
           <span
             className="text-slate-500 cursor-pointer absolute top-4 right-4 text-[27px]"
@@ -51,54 +48,57 @@ const Login = ({ handleClose }) => {
           >
             <MdClose />
           </span>
-         
         </div>
-        {isLoading ?
-       <div className="h-screen relative flex justify-center top-[100px] text-red-700">
-       <HashLoader color="#000000" />
-     </div>
-      
-      :  <form onSubmit={handleLogin}>
-          <div className="mb-4">
-            <label htmlFor="email" className="text-gray-700 block font-bold">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full mt-2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-cyan-500"
-              placeholder="Your email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+        {isLoading ? (
+          <div className="h-screen relative flex justify-center top-[100px] text-red-700">
+            <HashLoader color="#000000" />
           </div>
-          <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-700 font-bold">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="w-full mt-2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-cyan-500"
-              placeholder="Your password"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="text-white bg-cyan-500 px-4 py-2 rounded-md hover:bg-cyan-600 focus:outline-none focus:bg-cyan-600"
-            >
-              Sign In
-            </button>
-            <a href="#" className="text-cyan-500 hover:underline">
-              Forgot Password?
-            </a>
-          </div>
-        </form> }
+        ) : (
+          <form onSubmit={handleLogin}>
+            <div className="mb-4">
+              <label htmlFor="email" className="text-gray-700 block font-bold">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full mt-2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-slate-500"
+                placeholder="Your email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-bold"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full mt-2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-cyan-500"
+                placeholder="Your password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                type="submit"
+                className="text-white bg-slate-700 px-4 py-2 rounded-md hover:bg-slate-600 focus:outline-none focus:bg-cyan-600"
+              >
+                Sign In
+              </button>
+              <a href="#" className="text-slate-700 hover:underline">
+                Forgot Password?
+              </a>
+            </div>
+          </form>
+        )}
         {error && <p className="text-red-500 mt-4">{error}</p>}{" "}
       </div>
     </div>
--- src/components/auth/SignOut.jsx
+++ src/components/auth/SignOut.jsx
@@ -1,37 +1,37 @@
 import { useState } from "react";
-import { getAuth, signOut} from "firebase/auth";
-import { SlMenu } from "react-icons/sl"
+import { getAuth, signOut } from "firebase/auth";
+import { SlMenu } from "react-icons/sl";
 import { useNavigate } from "react-router-dom";
 
-
-
 const SignOut = () => {
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSignOut = async () => {
-        setIsLoading(true)
-        const auth = getAuth();
-        try {
-          await signOut(auth); 
-          setIsLoading(false)
-          navigate("/"); 
-        } catch (error) {
-          console.error("Sign out failed:", error);
-          setIsLoading(false)
-        }
-      };
-      
+  const handleSignOut = async () => {
+    setIsLoading(true);
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+      setIsLoading(false);
+      navigate("/");
+    } catch (error) {
+      console.error("Sign out failed:", error);
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div>
-        <div className="flex justify-between items-center mx-auto max-w-[1240px] px-4 pt-6 sm:pt-8  xl:px-0">
-        <button className="text-[16px] bg-slate-600 px-3 py-2 text-slate-100 rounded-md md:px-4 md:py-2 md:text-md" onClick={handleSignOut}>{`${isLoading ? "Login Out.." : "Log Out"}`}</button>
-        <SlMenu  className="text-3xl"/>
+      <div className="flex justify-between items-center mx-auto max-w-[1240px] px-4 pt-6 sm:pt-8  xl:px-0">
+        <button
+          className="text-[16px] bg-slate-600 px-3 py-2 text-slate-100 rounded-md md:px-4 md:py-2 md:text-md"
+          onClick={handleSignOut}
+        >{`${isLoading ? "Login Out.." : "Log Out"}`}</button>
+        <SlMenu className="text-3xl" />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SignOut;
